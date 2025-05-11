38a407fa786db1f95e9190864896782322097de2
kennatan <84018115+kennatan@users.noreply.github.com>
2025-05-11 11:38:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('伺服器啟動：http://localhost:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
